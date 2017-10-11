5b5652baf8a13f731e8a77dd6061f16af8450413
jwlh <jonnyhall@me.com>
2017-10-11 12:23:45 +0100

    added scrolling text bar to top

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 
+//things to do today
+//1. stop reset/play buttons working when playing back
+//2. add scrolling bar text instead of alerts for correct or incorrect answers
+//3. add mode where you can just play around with the synth
+//4. remove blue highlighting around play and rest buttons when you click
 
+// make buttons look grey by adding a class called hidden to all of them that makes the colour grey then remove that class when they are added and add it back in after a certain period of time
 
 $(() => {
 
@@ -10,14 +16,20 @@ $(() => {
   let currentScore = 0;
   let difficultyLevel = 1000;
 
+  enablePlay();
+  animateTicker();
 
 
   //function to start game
-
   function startGame() {
+    updateTicker('Let\'s play !!');
+    disablePlay();
     createAnsArray();
+    disableReset();
     playAnswer();
+    enableReset();
   }
+
   //get array of LIs from DOM and add their ID's to an array
   const $lisArray = $('li').map(function () {
     return this.id;
@@ -27,12 +39,10 @@ $(() => {
   $('h4').on('click', (e) => {
     $('h4').removeClass('neon');
     $(e.target).addClass('neon');
-  
     difficultyLevel = parseInt(e.target.id);
-
   });
 
-  //Add click event to each to add ID to user array
+  //Add click event to each li
   $('li').on('click', (e) => {
     //added audio to each li click event
     new Audio(`./Roland_TB-303/${e.target.id}.mp3`).play();
@@ -43,7 +53,6 @@ $(() => {
     },500);
     //added function to push each Li's ID into array when clicked
     playedArray.push(e.target.id);
-    console.log(playedArray);
     //then check if players array is the smae length as the answer array, if it is check they are the same or not
     if (ansArray.length === playedArray.length) {
       checkAnswer();
@@ -56,7 +65,6 @@ $(() => {
     for (let i=0; i<numberOfButtons; i++) {
       ansArray.push($lisArray[Math.floor(Math.random()*$lisArray.length)]);
     }
-    console.log(ansArray);
   }
 
   //function to play the sequence from the ansArray
@@ -84,21 +92,30 @@ $(() => {
   //function to check user answer
   function checkAnswer() {
     if (ansArray.toString() === playedArray.toString()){
-      console.log('correct');
       updateScore();
       numberOfButtons++;
       playedArray = [];
       ansArray.push($lisArray[Math.floor(Math.random()*$lisArray.length)]);
       console.log(ansArray);
+      disableReset();
       playAnswer();
-
+      enableReset();
     } else {
       alert('incorrect');
       resetGame();
     }
   }
+
   //clicking on play button event
-  $('#play').on('click', startGame);
+  function enablePlay(){
+    $('#play').on('click', startGame);
+  }
+
+  //disable clicking on play button event
+  function disablePlay(){
+    $('#play').off('click');
+  }
+
 
   //function to update score
   function updateScore() {
@@ -115,10 +132,20 @@ $(() => {
   }
 
   //click event for reset button
-  $('#reset').on('click', resetGame);
+  function enableReset(){
+    $('#reset').on('click', resetGame);
+  }
+
+  //click event to disable reset button
+  function disableReset(){
+    $('#reset').off('click');
+  }
+
+
 
   //reset game function
   function resetGame() {
+    enablePlay();
     playedArray = [];
     ansArray = [];
     numberOfButtons = 3;
@@ -126,42 +153,18 @@ $(() => {
     $('#currentScore').text(`Current Score = ${currentScore}`);
   }
 
+  // function to update text in ticker-wrap
+  function updateTicker(sample) {
+    $('.ticker-text').html(sample);
+    animateTicker();
+  }
 
-
-
+  //animation of ticker text
+  function animateTicker() {
+    $('.ticker-text').animate({
+      left: '420px'},1);
+    $('.ticker-text').animate({
+      left: '-1000px'
+    }, 10000, animateTicker);
+  }
 }); // end of waiting for DOM to load function
-
-// add click event to each button to push button id to an array
-// add sfx to each button so it makes noise when it clicks
-// create an array of all the button IDs (buttons can just have numbers as ID)  do this by pulling them in from DOM using jquery
-
-
-// game play
-
-//1. User clicks play button to start game
-//2. randomly select 4 buttons from the button array and add to play array
-//3. run through each button in play array and make it light up and play it's sfx (maybe this repeats 2/3 times?)  use set timeout to switch to light on version and then off for each square
-//4. collect the id of each button the user clicks on into an array.
-//5. once that has the same number of button ids as the play array stop and check the users array agains the play array
-//6. if it is the same update user score to the lenght of that array, add a new button ID to the end of the play array, clear the users array and loop back to step 2.  If incorect stop play and give user their score from the round before (i.e. the last time they got the sequence correct)
-
-// additional features
-
-//1. volume slider to look like mixing deck slider to adjust sound volume of alerts
-//2. difficluty selector to look like mixing nob.  difficluty selector adjusts speed which the computer plays through the sequence of buttons harder = quicker
-
-
-
-// const liArray = [];
-// let counter = 0;
-// setInterval(function() {
-//   liArray[counter].addClass('light');
-//   setTimeout(function() {
-//     liArray[counter].removeClass('light');
-//     counter++;
-//   }, 600);
-//
-//   if (counter === liArray.length) {
-//     clearInterval();
-//   }
-// }, 1000);
